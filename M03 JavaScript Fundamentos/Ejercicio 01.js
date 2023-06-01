1eb9a97ee13a4c4ca1c7e1fc0d0213761d36470a
marcosxld <marcososys097@gmail.com>
2023-05-31 22:26:25 -0500

    Hola papa

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -5,7 +5,7 @@
 // por el correspondiente.
 
 // Crea una variable de tipo string.
-const nuevoString = "nose";
+const nuevoString = "tutu";
 
 // Crea una variable de tipo number.
 const nuevoNumero = null;
